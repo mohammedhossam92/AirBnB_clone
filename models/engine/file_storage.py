55156a1d38193b058e17782955cc8d62218cf8a1
omar <0marleves99@gmail.com>
2023-12-10 15:22:53 -0800

    all

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python3
+"""importing models"""
 import json
 import os
 
 
 class FileStorage:
+    """file storage class"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """all function to store all"""
         return self.__objects
 
     def new(self, obj):
+        """new object added """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """save the data to the file json"""
         with open(self.__file_path, 'w') as f:  # Use 'w' mode for writing text
             json.dump(self.__objects, f, default=self.obj_to_dict)
 
     def reload(self):
+        """reload data from the file"""
         try:
             if os.path.exists(self.__file_path):
                 with open(self.__file_path, 'r') as f:
@@ -32,12 +38,3 @@ class FileStorage:
         if hasattr(obj, 'to_dict'):
             return obj.to_dict()
         return obj.__dict__
-
-
-# Sample usage:
-class SampleModel:
-    def __init__(self, id):
-        self.id = id
-
-    def to_dict(self):
-        return {'id': self.id}
