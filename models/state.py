e910ec25fd02184359a3ad5cd7fc2b76e807a457
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-12-10 17:52:48 -0800

    all

--- models/state.py
+++ models/state.py
@@ -5,9 +5,4 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """class state that contain the data related to class"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = ""
-
-    def __str__(self):
-        return "[State] ({}) {}".format(self.id, self.name)
+    name = ''
