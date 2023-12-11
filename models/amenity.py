b641efd53610a026eb6ef51d8da9113f856b4459
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-12-10 17:49:51 -0800

    all

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
+"""importing module """
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
     """class to define the Amenity"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.name = ""
-
-    def __str__(self):
-        return "[Amenity] ({}) {}".format(self.id, self.name)
+    name = ''
