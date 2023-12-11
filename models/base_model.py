f8796840499117079c1589c2cedaa1fa141754f2
mohammed hossam <mohammedhossam5000@gmail.com>
2023-12-12 01:03:41 +0200

    all

--- models/base_model.py
+++ models/base_model.py
@@ -58,6 +58,7 @@ class BaseModel:
         """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.utcnow()
         models.storage.save()
+        return True
 
     def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
