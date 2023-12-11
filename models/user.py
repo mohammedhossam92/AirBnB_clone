8bbf296b12e87565efc484322934688717862c94
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-12-10 17:46:17 -0800

    all

--- models/user.py
+++ models/user.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
+"""importing module"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """class user contain user details"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
-
-    def __str__(self):
-        return "[User] ({}) {} {}".format(self.id,
-                                          self.first_name, self.last_name)
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
