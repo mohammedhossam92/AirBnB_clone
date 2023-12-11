e7ac264067c7eb3ddbd601c3434ee3113050c6b6
mohammed hossam <mohammedhossam5000@gmail.com>
2023-12-11 19:32:05 +0200

    all

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,67 +1,74 @@
 #!/usr/bin/python3
+# Import the unittest module and the Place class
 import unittest
-from datetime import datetime
 from models.place import Place
+from models.base_model import BaseModel
 
 
+# Create a subclass of unittest.TestCase
 class TestPlace(unittest.TestCase):
-    def test_init(self):
-        """
-        Test the __init__ method of the Place class
-        """
-        place = Place()
-        self.assertIsInstance(place, Place)
-        self.assertTrue(hasattr(place, 'id'))
-        self.assertTrue(hasattr(place, 'created_at'))
-        self.assertTrue(hasattr(place, 'updated_at'))
-        self.assertIsInstance(place.id, str)
-        self.assertIsInstance(place.created_at, datetime)
-        self.assertIsInstance(place.updated_at, datetime)
-        self.assertTrue(hasattr(place, 'city_id'))
-        self.assertEqual(place.city_id, "")
-        self.assertTrue(hasattr(place, 'user_id'))
-        self.assertEqual(place.user_id, "")
-        self.assertTrue(hasattr(place, 'name'))
-        self.assertEqual(place.name, "")
-        self.assertTrue(hasattr(place, 'description'))
-        self.assertEqual(place.description, "")
-        self.assertTrue(hasattr(place, 'number_rooms'))
-        self.assertEqual(place.number_rooms, 0)
-        self.assertTrue(hasattr(place, 'number_bathrooms'))
-        self.assertEqual(place.number_bathrooms, 0)
-        self.assertTrue(hasattr(place, 'max_guest'))
-        self.assertEqual(place.max_guest, 0)
-        self.assertTrue(hasattr(place, 'price_by_night'))
-        self.assertEqual(place.price_by_night, 0)
-        self.assertTrue(hasattr(place, 'latitude'))
-        self.assertEqual(place.latitude, 0.0)
-        self.assertTrue(hasattr(place, 'longitude'))
-        self.assertEqual(place.longitude, 0.0)
-        self.assertTrue(hasattr(place, 'amenity_ids'))
-        self.assertEqual(place.amenity_ids, [])
 
-    def test_str(self):
-        """
-        Test the __str__ method of the Place class
-        """
-        place = Place()
-        str_representation = str(place)
-        self.assertIn("[Place]", str_representation)
-        self.assertIn("'id':", str_representation)
-        self.assertIn("'created_at':", str_representation)
-        self.assertIn("'updated_at':", str_representation)
-        self.assertIn("'city_id':", str_representation)
-        self.assertIn("'user_id':", str_representation)
-        self.assertIn("'name':", str_representation)
+    # Use the setUp method to create an instance of the Place class
+    # and assign some values to its attributes
+    def setUp(self):
+        self.place = Place()
+        self.place.city_id = "1"
+        self.place.user_id = "2"
+        self.place.name = "My place"
+        self.place.description = "A cozy apartment"
+        self.place.number_rooms = 2
+        self.place.number_bathrooms = 1
+        self.place.max_guest = 4
+        self.place.price_by_night = 100
+        self.place.latitude = 40.0
+        self.place.longitude = -3.0
+        self.place.amenity_ids = ["3", "4"]
 
-    def test_name(self):
-        """
-        Test setting the name attribute of the Place class
-        """
-        place = Place()
-        place.name = "Cozy Cottage"
-        self.assertEqual(place.name, "Cozy Cottage")
+    # Use the assert methods to check the expected behavior of the
+    # Place class and its attributes
+    def test_place(self):
+        # Check that the place instance is an object of the Place class
+        self.assertIsInstance(self.place, Place)
+        # Check that the place instance inherits from the BaseModel class
+        self.assertTrue(issubclass(self.place.__class__, BaseModel))
+        # Check that the place instance has the eleven attributes
+        self.assertTrue(hasattr(self.place, "city_id"))
+        self.assertTrue(hasattr(self.place, "user_id"))
+        self.assertTrue(hasattr(self.place, "name"))
+        self.assertTrue(hasattr(self.place, "description"))
+        self.assertTrue(hasattr(self.place, "number_rooms"))
+        self.assertTrue(hasattr(self.place, "number_bathrooms"))
+        self.assertTrue(hasattr(self.place, "max_guest"))
+        self.assertTrue(hasattr(self.place, "price_by_night"))
+        self.assertTrue(hasattr(self.place, "latitude"))
+        self.assertTrue(hasattr(self.place, "longitude"))
+        self.assertTrue(hasattr(self.place, "amenity_ids"))
+        # Check that the attributes have the correct types
+        self.assertIsInstance(self.place.city_id, str)
+        self.assertIsInstance(self.place.user_id, str)
+        self.assertIsInstance(self.place.name, str)
+        self.assertIsInstance(self.place.description, str)
+        self.assertIsInstance(self.place.number_rooms, int)
+        self.assertIsInstance(self.place.number_bathrooms, int)
+        self.assertIsInstance(self.place.max_guest, int)
+        self.assertIsInstance(self.place.price_by_night, int)
+        self.assertIsInstance(self.place.latitude, float)
+        self.assertIsInstance(self.place.longitude, float)
+        self.assertIsInstance(self.place.amenity_ids, list)
+        # Check that the attributes have the correct values
+        self.assertEqual(self.place.city_id, "1")
+        self.assertEqual(self.place.user_id, "2")
+        self.assertEqual(self.place.name, "My place")
+        self.assertEqual(self.place.description, "A cozy apartment")
+        self.assertEqual(self.place.number_rooms, 2)
+        self.assertEqual(self.place.number_bathrooms, 1)
+        self.assertEqual(self.place.max_guest, 4)
+        self.assertEqual(self.place.price_by_night, 100)
+        self.assertEqual(self.place.latitude, 40.0)
+        self.assertEqual(self.place.longitude, -3.0)
+        self.assertEqual(self.place.amenity_ids, ["3", "4"])
 
 
-if __name__ == '__main__':
+# Use the unittest.main function to run the tests
+if __name__ == "__main__":
     unittest.main()
