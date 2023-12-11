86bbe050279d5040cd9d94c4b3fce9aaa1f54dd9
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-12-10 17:51:34 -0800

    all

--- models/city.py
+++ models/city.py
@@ -5,7 +5,5 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """city class contain all data related to city"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
+    state_id = ''
+    name = ''
