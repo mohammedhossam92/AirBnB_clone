bff94612b777ceca82696a9dd73c11aa1f60422c
Mohammed Hossam <mohammedhossam5000@gmail.com>
2023-12-10 17:54:35 -0800

    all

--- models/review.py
+++ models/review.py
@@ -5,11 +5,6 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """class review that contain the data related to review"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
-
-    def __str__(self):
-        return "[Review] ({}) {}".format(self.id, self.text)
+    place_id = ''
+    user_id = ''
+    test = ''
