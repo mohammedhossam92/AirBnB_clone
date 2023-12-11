b3867f334c59524901286753b481808477675ad1
mohammed hossam <mohammedhossam5000@gmail.com>
2023-12-11 17:40:19 +0200

    all

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -4,65 +4,96 @@ import unittest
 import json
 import os
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
+# from models.amenity import Amenity
+# from models.city import City
 from models.place import Place
-from models.review import Review
-from models.state import State
+# from models.review import Review
+# from models.state import State
 from models.user import User
 from models.engine.file_storage import FileStorage
 
+# Create a subclass of unittest.TestCase
+
 
 class TestFileStorage(unittest.TestCase):
-    """class test file"""
+
+    # Use the setUp method to create an instance
+    # of FileStorage and some sample objects
     def setUp(self):
-        """class to test"""
-        self.file_path = "test_file.json"
         self.storage = FileStorage()
-        self.storage._FileStorage__file_path = self.file_path
+        self.obj1 = BaseModel()
+        self.obj2 = User()
+        self.obj3 = Place()
 
-    def tearDown(self):
-        """class testt tear"""
-        if os.path.exists(self.file_path):
-            os.remove(self.file_path)
+    # Use the assert methods to check the expected
+    # behavior of the FileStorage methods and variables
+    def test_all(self):
+        # Check that the all method returns a dictionary of objects
+        self.assertIsInstance(self.storage.all(), dict)
+        # Check that the dictionary is initially empty
+        self.assertEqual(len(self.storage.all()), 0)
 
-    def test_all_empty(self):
-        """class test test """
-        result = self.storage.all()
-        self.assertEqual(result, {})
+    def test_new(self):
+        # Check that the new method adds an object to the __objects dictionary
+        self.storage.new(self.obj1)
+        self.assertIn("BaseModel.{}".format(self.obj1.id), self.storage.all())
+        self.storage.new(self.obj2)
+        self.assertIn("User.{}".format(self.obj2.id), self.storage.all())
+        self.storage.new(self.obj3)
+        self.assertIn("Place.{}".format(self.obj3.id), self.storage.all())
+        # Check that the new method does nothing if the object is None
+        self.storage.new(None)
+        self.assertEqual(len(self.storage.all()), 3)
 
-    def test_new_object_added(self):
-        """class test user """
-        user = User()
-        self.storage.new(user)
-        result = self.storage.all()
-        self.assertEqual(len(result), 1)
-        key = "{}.{}".format(user.__class__.__name__, user.id)
-        self.assertIn(key, result)
-
-    def test_save_and_reload(self):
-        """class test sava and reload """
-        user = User()
-        self.storage.new(user)
+    def test_save(self):
+        # Check that the save method creates a file.json file
         self.storage.save()
+        self.assertTrue(os.path.exists("file.json"))
+        # Check that the file.json file contains the correct data
+        with open("file.json", "r") as f:
+            data = json.load(f)
+            self.assertEqual(data, self.storage.all())
 
-        new_storage = FileStorage()
-        new_storage._FileStorage__file_path = self.file_path
-        new_storage.reload()
-
-        result = new_storage.all()
-        self.assertEqual(len(result), 1)
-        key = "{}.{}".format(user.__class__.__name__, user.id)
-        self.assertIn(key, result)
+    def test_reload(self):
+        # Check that the reload method loads the data from the file.json file
+        self.storage.reload()
+        self.assertEqual(self.storage.all(), {})
+        self.storage.new(self.obj1)
+        self.storage.new(self.obj2)
+        self.storage.new(self.obj3)
+        self.storage.save()
+        self.storage.reload()
+        self.assertEqual(len(self.storage.all()), 3)
+        # Check that the reload method creates
+        # the correct objects from the data
+        for key, value in self.storage.all().items():
+            self.assertIsInstance(value, eval(value.__class__.__name__))
+        # Check that the reload method does
+        # nothing if the file.json file does not exist
+        os.remove("file.json")
+        self.storage.reload()
+        self.assertEqual(len(self.storage.all()), 3)
 
     def test_obj_to_dict(self):
-        """class test obj """
-        user = User()
-        result = self.storage.obj_to_dict(user)
-        expected = user.to_dict() if hasattr(user, 'to_dict') \
-            else user.__dict__
-        self.assertEqual(result, expected)
+        # Check that the obj_to_dict method
+        # returns a dictionary representation of an object
+        self.assertIsInstance(self.storage.obj_to_dict(self.obj1), dict)
+        self.assertEqual(self.storage.obj_to_dict(self.obj1),
+                         self.obj1.to_dict())
+        # Check that the obj_to_dict method
+        # returns the __dict__ attribute of an object
+        # if it does not have a to_dict method
+        self.assertIsInstance(self.storage.obj_to_dict(self.storage), dict)
+        self.assertEqual(self.storage.obj_to_dict(self.storage),
+                         self.storage.__dict__)
+
+    # Use the tearDown method to delete the file.json file after each test
+    def tearDown(self):
+        if os.path.exists("file.json"):
+            os.remove("file.json")
+
+# Use the unittest.main function to run the tests
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
