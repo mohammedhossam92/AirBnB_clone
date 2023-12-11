c28a2c8c3a43050790df3f6eed562bf03cac5426
mohammed hossam <mohammedhossam5000@gmail.com>
2023-12-11 19:01:13 +0200

    all

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,46 +1,45 @@
-#!/usr/bin/python3
+# Import the unittest module and the User class
 import unittest
-from datetime import datetime  # Importing the datetime module
 from models.user import User
+from models.base_model import BaseModel
 
 
+# Create a subclass of unittest.TestCase
 class TestUser(unittest.TestCase):
-    def test_init(self):
-        """Test the initialization of the User instance."""
-        user = User()
-        self.assertIsInstance(user, User)
-        self.assertTrue(hasattr(user, 'id'))
-        self.assertTrue(hasattr(user, 'created_at'))
-        self.assertTrue(hasattr(user, 'updated_at'))
-        self.assertIsInstance(user.id, str)
-        self.assertIsInstance(user.created_at, datetime)
-        self.assertIsInstance(user.updated_at, datetime)
-        self.assertTrue(hasattr(user, 'email'))
-        self.assertTrue(hasattr(user, 'password'))
-        self.assertTrue(hasattr(user, 'first_name'))
-        self.assertTrue(hasattr(user, 'last_name'))
-        self.assertEqual(user.email, "")
-        self.assertEqual(user.password, "")
-        self.assertEqual(user.first_name, "")
-        self.assertEqual(user.last_name, "")
 
-    def test_str(self):
-        """Test the __str__ method of the User instance."""
-        user = User()
-        user.first_name = "John"
-        user.last_name = "Doe"
-        str_representation = str(user)
-        self.assertIn("[User]", str_representation)
-        self.assertIn("'id':", str_representation)
-        self.assertIn("'created_at':", str_representation)
-        self.assertIn("'updated_at':", str_representation)
-        self.assertIn("'email':", str_representation)
-        self.assertIn("'password':", str_representation)
-        self.assertIn("'first_name':", str_representation)
-        self.assertIn("'last_name':", str_representation)
-        self.assertIn("John", str_representation)
-        self.assertIn("Doe", str_representation)
+    # Use the setUp method to create an instance
+    # of the User class and assign some values to its attributes
+    def setUp(self):
+        self.user = User()
+        self.user.email = "user@example.com"
+        self.user.password = "123456"
+        self.user.first_name = "John"
+        self.user.last_name = "Doe"
+
+    # Use the assert methods to check the expected
+    # behavior of the User class and its attributes
+    def test_user(self):
+        # Check that the user instance is an object of the User class
+        self.assertIsInstance(self.user, User)
+        # Check that the user instance inherits from the BaseModel class
+        self.assertTrue(issubclass(self.user.__class__, BaseModel))
+        # Check that the user instance has the four attributes
+        self.assertTrue(hasattr(self.user, "email"))
+        self.assertTrue(hasattr(self.user, "password"))
+        self.assertTrue(hasattr(self.user, "first_name"))
+        self.assertTrue(hasattr(self.user, "last_name"))
+        # Check that the attributes are strings
+        self.assertIsInstance(self.user.email, str)
+        self.assertIsInstance(self.user.password, str)
+        self.assertIsInstance(self.user.first_name, str)
+        self.assertIsInstance(self.user.last_name, str)
+        # Check that the attributes have the correct values
+        self.assertEqual(self.user.email, "user@example.com")
+        self.assertEqual(self.user.password, "123456")
+        self.assertEqual(self.user.first_name, "John")
+        self.assertEqual(self.user.last_name, "Doe")
 
 
-if __name__ == '__main__':
+# Use the unittest.main function to run the tests
+if __name__ == "__main__":
     unittest.main()
