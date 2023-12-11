17d19d2061356c4645fc8051a7463974f095c21c
mohammed hossam <mohammedhossam5000@gmail.com>
2023-12-11 19:16:23 +0200

    all

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,50 +1,38 @@
 #!/usr/bin/python3
+# Import the unittest module and the City class
 import unittest
-from datetime import datetime
 from models.city import City
+from models.base_model import BaseModel
 
 
+# Create a subclass of unittest.TestCase
 class TestCity(unittest.TestCase):
-    """Test cases for the City class."""
 
-    def test_init(self):
-        """Test the initialization of the City instance."""
-        city = City()
-        self.assertIsInstance(city, City)
-        self.assertTrue(hasattr(city, 'id'))
-        self.assertTrue(hasattr(city, 'created_at'))
-        self.assertTrue(hasattr(city, 'updated_at'))
-        self.assertIsInstance(city.id, str)
-        self.assertIsInstance(city.created_at, datetime)
-        self.assertIsInstance(city.updated_at, datetime)
-        self.assertTrue(hasattr(city, 'state_id'))
-        self.assertEqual(city.state_id, "")
-        self.assertTrue(hasattr(city, 'name'))
-        self.assertEqual(city.name, "")
+    # Use the setUp method to create an instance of the City class
+    # and assign some values to its attributes
+    def setUp(self):
+        self.city = City()
+        self.city.state_id = "1"
+        self.city.name = "New York"
 
-    def test_str(self):
-        """Test the __str__ method of the City instance."""
-        city = City()
-        str_representation = str(city)
-        self.assertIn("[City]", str_representation)
-        self.assertIn("'id':", str_representation)
-        self.assertIn("'created_at':", str_representation)
-        self.assertIn("'updated_at':", str_representation)
-        self.assertIn("'state_id':", str_representation)
-        self.assertIn("'name':", str_representation)
-
-    def test_state_id(self):
-        """Test the 'state_id' attribute of the City instance."""
-        city = City()
-        city.state_id = "CA"
-        self.assertEqual(city.state_id, "CA")
-
-    def test_name(self):
-        """Test the 'name' attribute of the City instance."""
-        city = City()
-        city.name = "San Francisco"
-        self.assertEqual(city.name, "San Francisco")
+    # Use the assert methods to check the expected behavior of the City
+    # class and its attributes
+    def test_city(self):
+        # Check that the city instance is an object of the City class
+        self.assertIsInstance(self.city, City)
+        # Check that the city instance inherits from the BaseModel class
+        self.assertTrue(issubclass(self.city.__class__, BaseModel))
+        # Check that the city instance has the two attributes
+        self.assertTrue(hasattr(self.city, "state_id"))
+        self.assertTrue(hasattr(self.city, "name"))
+        # Check that the attributes are strings
+        self.assertIsInstance(self.city.state_id, str)
+        self.assertIsInstance(self.city.name, str)
+        # Check that the attributes have the correct values
+        self.assertEqual(self.city.state_id, "1")
+        self.assertEqual(self.city.name, "New York")
 
 
-if __name__ == '__main__':
+# Use the unittest.main function to run the tests
+if __name__ == "__main__":
     unittest.main()
